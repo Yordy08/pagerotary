af0a57c9119be20e622f4e600a56d945a91cd6b8
Yordy08 <yordisdurango@gmail.com>
2024-10-16 15:06:10 -0500

    .....

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
 
     runtimeConfig: {
         public: {
-            MONGODB_URI: 'mongodb://localhost:27017/',
+            MONGODB_URI: 'mongodb+srv://yordisdurango:zFgn8AsIBTI1rw03@cluster0.y6f8p.mongodb.net/',
         }
     },
     routeRules: {
