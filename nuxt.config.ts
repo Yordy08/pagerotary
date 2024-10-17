c1fb5f4f76d48092335cf8fd8afa74403700ceac
Carjul <emugameplay.tv@gmail.com>
2024-10-16 23:23:52 -0500

    Refactor API endpoints for Noticias and add file upload functionality

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,14 @@
 export default defineNuxtConfig({
-    compatibilityDate: '2024-01-01',
+    compatibilityDate: '2024-04-03',
+
     runtimeConfig: {
         public: {
-         MONGODB_URI: 'mongodb+srv://yordisdurango:zFgn8AsIBTI1rw03@cluster0.y6f8p.mongodb.net/',
+         //MONGODB_URI: 'mongodb+srv://yordisdurango:zFgn8AsIBTI1rw03@cluster0.y6f8p.mongodb.net/',
          // estas son las mias usar una cuenta propia
          CLOUDINARY_CLOUD_NAME: 'dn4xdl2v5',
          CLOUDINARY_API_KEY: '357585198847445',
          CLOUDINARY_API_SECRET: 'odYQsXNPvjMByG800ToDDoEajlY',
-
-         // MONGODB_URI: 'mongodb://localhost:27017/',
+         MONGODB_URI: 'mongodb://localhost:27017/',
         }
     },
     routeRules: {
--- pages/about.vue
+++ pages/about.vue
@@ -11,3 +11,33 @@
 <style>
 
 </style>
+
+<!-- En tu componente Vue -->
+<!-- <template>
+  <form @submit.prevent="submit">
+    <input type="file" @change="selectFile" />
+    <button type="submit">Subir Imagen</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const selectedFile = ref(null)
+
+const selectFile = (event) => {
+  selectedFile.value = event.target.files[0]
+}
+
+const submit = async () => {
+  const formData = new FormData()
+  formData.append('photo', selectedFile.value)
+
+  const response = await fetch('/api/Upload/imagen', {
+    method: 'POST',
+    body: formData
+  })
+
+  const data = await response.json()
+  console.log('->:', data)
+} </script> -->
