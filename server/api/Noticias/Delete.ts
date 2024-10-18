a6b8d164f20409a618a7b471470f17eadf065727
Carjul <emugameplay.tv@gmail.com>
2024-10-18 16:22:51 -0500

    fix update user

--- server/api/Noticias/Delete.ts
+++ server/api/Noticias/Delete.ts
@@ -1,6 +1,7 @@
 import { defineEventHandler, readBody } from "h3";
 import { Noticia } from "../../models/Noticia";
 import { connect, getDatabase, getCollection } from '../../utils/mongodb';
+import { ObjectId } from "mongodb";
 
 export default defineEventHandler(async (event) => {
     try {
@@ -8,12 +9,18 @@ export default defineEventHandler(async (event) => {
 
             const { _id } = await readBody(event);
 
-
+            if (!ObjectId.isValid(_id)) {
+                throw createError({
+                    statusCode: 400,
+                    statusMessage: 'Invalid ID format',
+                });
+            }
             const client = await connect();
             const db = await getDatabase(client);
             const collection = await getCollection<Noticia>(db, 'noticias');
 
-            const result = await collection.deleteOne({ _id: _id });
+            const result = await collection.deleteOne({ _id: new ObjectId(_id) });
+    
 
             if (result.deletedCount === 1) {
                 return {
--- server/api/Noticias/Update.ts
+++ server/api/Noticias/Update.ts
@@ -1,43 +1,51 @@
-import { defineEventHandler, readBody } from "h3";
+import { defineEventHandler, readBody, createError } from "h3";
 import { Noticia } from "../../models/Noticia";
 import { connect, getDatabase, getCollection } from '../../utils/mongodb';
+import { ObjectId } from "mongodb";
 
 export default defineEventHandler(async (event) => {
     try {
         if (event.method === 'PUT') {
             const noticia: Noticia = await readBody(event);
+            const { _id, ...updateData } = noticia;
+
+            if (!_id) {
+                throw createError({
+                    statusCode: 400,
+                    statusMessage: 'ID de la noticia es requerido',
+                });
+            }
 
             const client = await connect();
             const db = await getDatabase(client);
             const collection = await getCollection<Noticia>(db, 'noticias');
 
             const result = await collection.updateOne(
-                { _id: noticia._id },
-                { $set: noticia }
+                { _id: new ObjectId(noticia._id) },
+                { $set: updateData }
             );
 
             if (result.modifiedCount > 0) {
                 return {
                     statusCode: 200,
-                    body: { Message: 'Noticia actualizada' }
+                    Message: 'Noticia actualizada'
                 };
             } else {
-                return {
+                throw createError({
                     statusCode: 404,
-                    body:{ Message: 'Noticia no encontrada' }
-                };
+                    statusMessage: 'Noticia no encontrada'
+                });
             }
-        }else{
+        } else {
             throw createError({
                 statusCode: 405,
                 statusMessage: 'Method Not Allowed',
             });
         }
-
     } catch (error) {
         return {
             statusCode: 500,
-            body:{ Message: 'Error interno del servidor', Error: error }
+            body: { Message: 'Error interno del servidor', Error: error },
         };
     }
 });
